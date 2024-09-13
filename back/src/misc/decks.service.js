927148d215244134032b5653c71e3e9346ad5da6
niko <diablade@hotmail.com>
2024-09-13 18:48:31 +0200

    chore : improve deck service

--- back/src/misc/decks.service.js
+++ back/src/misc/decks.service.js
@@ -6,38 +6,40 @@ import log from "../../config/log.js";
 const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const colors = ["red", "yellow", "green", "blue"];
 
-
-async function generateOneCard(letter, color, weight, price) {
+const areCardIdsUnique = (cardIds, authorizedLength) => {
+	const uniqueIds = new Set(cardIds);  // Convert array to a Set to eliminate duplicates
+	return uniqueIds.size === authorizedLength;  // Compare the size of the Set array length authorized
+}
+const generateOneCard = async (letter, color, weight, price) => {
 	const comId = new mongoose.Types.ObjectId();
 	let card = constructor.card(letter, color, weight, price);
 	card._id = comId;
 	return card;
 }
+const generateDecks = async (game) => {
+	const prices = [game.priceWeight1, game.priceWeight2, game.priceWeight3, game.priceWeight4];
 
-export default {
-	async generateDecks(game) {
-		const prices = [game.priceWeight1, game.priceWeight2, game.priceWeight3, game.priceWeight4];
-
-		let tableDecks = [[], [], [], []];
-		let lettersInGame = game.players.length;
-		if (!game.generateLettersAuto) {
-			lettersInGame = game.generateLettersInDeck;
-		}
-		// genere cartes pour les 4 lots
-		for (let weight = 0; weight <= 3; weight++) {
-			let deck = [];
-			for (let letter = 0; letter <= lettersInGame; letter++) {
-				// genere 3, 4 ou 5 cartes identiques
-				for (let j = 1; j <= game.generatedIdenticalCards; j++) {
-					const card = await generateOneCard(letters[letter], colors[weight], weight, prices[weight]);
-					deck.push(card);
-				}
+	let tableDecks = [[], [], [], []];
+	let lettersInGame = game.players.length;
+	if (!game.generateLettersAuto) {
+		lettersInGame = game.generateLettersInDeck;
+	}
+	// genere cartes pour les 4 lots
+	for (let weight = 0; weight <= 3; weight++) {
+		let deck = [];
+		for (let letter = 0; letter <= lettersInGame; letter++) {
+			// genere 3, 4 ou 5 cartes identiques
+			for (let j = 1; j <= game.generatedIdenticalCards; j++) {
+				const card = await generateOneCard(letters[letter], colors[weight], weight, prices[weight]);
+				deck.push(card);
 			}
-			tableDecks[weight] = _.shuffle(deck);
 		}
-		return tableDecks;
-	},
-	async pushCardsInDecks(idGame, cards) {
+		tableDecks[weight] = _.shuffle(deck);
+	}
+	return tableDecks;
+}
+const pushCardsInDecks = async (idGame, cards) => {
+	try {
 		const groupedCards = _.groupBy(_.sortBy(cards, 'weight'), 'weight');
 		await GameModel.updateOne(
 			{_id: idGame},
@@ -49,8 +51,15 @@ export default {
 					[`decks.${3}`]: {$each: groupedCards[3] ? groupedCards[3] : []},
 				},
 			}
-		).catch(err => {
-			log.error('cards are not back in decks, error', err);
-		});
+		);
+	} catch (err) {
+		log.error("Cards are not back in decks");
+		return (err);
 	}
+}
+
+export default {
+	generateDecks: generateDecks,
+	pushCardsInDecks: pushCardsInDecks,
+	areCardIdsUnique: areCardIdsUnique,
 }
