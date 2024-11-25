f9f91aea03a3f26ccd3ec827e220d157a5d3d6a3
niko <diablade@hotmail.com>
2024-11-26 00:33:41 +0100

    feat: start translation

---
+++ front/src/app/components/language-btn/language-btn.component.ts
@@ -0,0 +1,28 @@
+import {Component, Input} from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
+
+@Component({
+	selector: 'app-language-btn',
+	templateUrl: './language-btn.component.html',
+	styleUrls: ['./language-btn.component.scss']
+})
+export class LanguageBtnComponent {
+	languages = [
+		{flag: "🇫🇷", language: "Français", lang: "fr"},
+		{flag: "🇪🇸", language: "Spanish", lang: "es"},
+		{flag: "🇮🇹", language: "Italiano", lang: "it"},
+		{flag: "🇬🇧", language: "English", lang: "en"}
+	];
+	selectedLanguage: any;
+	@Input() short: boolean = false;
+
+	constructor(private translate: TranslateService) {
+		this.selectedLanguage = this.languages.find(l => l.lang === this.translate.currentLang);
+	}
+
+	switchLanguage(language: any) {
+		this.selectedLanguage = language;
+		this.translate.use(language.lang);
+		localStorage.setItem('language', language.lang); // Save preference
+	}
+}
