83b6ccfb71dce856807f281ff681724b1a3887ec
niko <diablade@hotmail.com>
2024-12-05 18:44:21 +0100

    chore : contract dialog part1

--- front/src/app/dialogs/contract-dialog/contract-dialog.component.ts
+++ front/src/app/dialogs/contract-dialog/contract-dialog.component.ts
@@ -4,32 +4,38 @@ import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {faFileSignature} from "@fortawesome/free-solid-svg-icons";
 // @ts-ignore
 import * as C from "../../../../../config/constantes";
+import * as _ from 'lodash-es';
+
 
 @Component({
-  selector: 'app-contract-dialog',
-  templateUrl: './contract-dialog.component.html',
-  styleUrls: ['./contract-dialog.component.scss']
+	selector: 'app-contract-dialog',
+	templateUrl: './contract-dialog.component.html',
+	styleUrls: ['./contract-dialog.component.scss']
 })
 export class ContractDialogComponent {
 
-  faFileSignature = faFileSignature;
-  game: Game = new Game();
-  C = C;
-  idPlayer: Player | undefined;
+	faFileSignature = faFileSignature;
+	game: Game = new Game();
+	C = C;
+	idPlayer: Player | undefined;
 
-  constructor(public dialogRef: MatDialogRef<ContractDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
-    this.game = data.game;
-  }
+	constructor(public dialogRef: MatDialogRef<ContractDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
+		this.game = data.game;
+	}
 
-  cancel() {
-    this.dialogRef.close();
-  }
+	cancel() {
+		this.dialogRef.close();
+	}
 
-  saveUserCredit() {
-    this.dialogRef.close({
-      idPlayer: this.idPlayer,
-      amount: this.game.defaultCreditAmount,
-      interest: this.game.defaultInterestAmount
-    });
-  }
+	getAlivePlayer(): Player[] {
+		return _.filter(this.game.players, p => p.status === C.ALIVE);
+	}
+
+	saveUserCredit() {
+		this.dialogRef.close({
+			idPlayer: this.idPlayer,
+			amount: this.game.defaultCreditAmount,
+			interest: this.game.defaultInterestAmount
+		});
+	}
 }
