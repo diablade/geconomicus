20e9d6ac85c603d5bffa3a2123cb1c4e96fd40c7
niko <diablade@hotmail.com>
2025-05-09 22:42:44 +0200

    fix: regression delete game

--- front/src/app/services/snackbar.service.ts
+++ front/src/app/services/snackbar.service.ts
@@ -2,58 +2,57 @@ import {Injectable} from '@angular/core';
 import {MatSnackBar, MatSnackBarConfig} from '@angular/material/snack-bar';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class SnackbarService {
 
-  durationInSeconds = 3000;
-  durationInSecondsReload = 7000;
+	durationInSeconds = 3000;
+	durationInSecondsReload = 7000;
 
-  constructor(private snackBar: MatSnackBar) {
-  }
+	constructor(private snackBar: MatSnackBar) {
+	}
 
-  showSuccess(message: string) {
-    const config = new MatSnackBarConfig();
-    config.panelClass = ['snackbar-success'];
-    config.politeness = 'assertive';
-    config.verticalPosition = 'top';
-    config.horizontalPosition = 'right';
-    config.duration = this.durationInSeconds;
-    this.snackBar.open(message, undefined, config);
-  }
+	showSuccess(message: string) {
+		const config = new MatSnackBarConfig();
+		config.panelClass = ['snackbar-success'];
+		config.politeness = 'assertive';
+		config.verticalPosition = 'top';
+		config.horizontalPosition = 'right';
+		config.duration = this.durationInSeconds;
+		this.snackBar.open(message, undefined, config);
+	}
 
-  showNotif(message: string) {
-    const config = new MatSnackBarConfig();
-    config.panelClass = ['snackbar-success'];
-    config.politeness = 'assertive';
-    config.verticalPosition = 'top';
-    config.horizontalPosition = 'left';
-    config.duration = this.durationInSeconds;
-    this.snackBar.open(message, "Ok", config);
-  }
+	showNotif(message: string) {
+		const config = new MatSnackBarConfig();
+		config.panelClass = ['snackbar-success'];
+		config.politeness = 'assertive';
+		config.verticalPosition = 'top';
+		config.horizontalPosition = 'left';
+		config.duration = this.durationInSeconds;
+		this.snackBar.open(message, "Ok", config);
+	}
 
+	showError(message: string) {
+		const config = new MatSnackBarConfig();
+		config.panelClass = ['snackbar-error'];
+		config.politeness = 'assertive';
+		config.verticalPosition = 'top';
+		config.horizontalPosition = 'center';
+		config.duration = this.durationInSeconds;
+		this.snackBar.open(message, undefined, config);
+	}
 
-  showError(message: string) {
-    const config = new MatSnackBarConfig();
-    config.panelClass = ['snackbar-error'];
-    config.politeness = 'assertive';
-    config.verticalPosition = 'top';
-    config.horizontalPosition = 'center';
-    config.duration = this.durationInSeconds;
-    this.snackBar.open(message, undefined, config);
-  }
-  showReload(message: string) {
-    const config = new MatSnackBarConfig();
-    config.panelClass = ['snackbar-error'];
-    config.politeness = 'assertive';
-    config.verticalPosition = 'top';
-    config.horizontalPosition = 'center';
-    config.duration = this.durationInSecondsReload;
-    const snackBarRef = this.snackBar.open(message, "🔄", config);
+	showReload(message: string) {
+		const config = new MatSnackBarConfig();
+		config.panelClass = ['snackbar-error'];
+		config.politeness = 'assertive';
+		config.verticalPosition = 'top';
+		config.horizontalPosition = 'center';
+		config.duration = this.durationInSecondsReload;
+		const snackBarRef = this.snackBar.open(message, "🔄", config);
 
-    snackBarRef.onAction().subscribe(() => {
-      window.location.reload();
-    });
-    
-  }
+		snackBarRef.onAction().subscribe(() => {
+			window.location.reload();
+		});
+	}
 }
