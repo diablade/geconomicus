ffc00506ce84c7769f41af3906a5be9cde53e190
niko <diablade@hotmail.com>
2025-05-10 02:26:37 +0200

    fix: auto btn click to clean interval properly

--- front/src/app/components/btn-timer-auto-click/btn-timer-auto-click.component.ts
+++ front/src/app/components/btn-timer-auto-click/btn-timer-auto-click.component.ts
@@ -1,47 +1,62 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 
 @Component({
-  selector: 'button-timer',
-  templateUrl: './btn-timer-auto-click.component.html',
-  styleUrls: ['./btn-timer-auto-click.component.scss']
+	selector: 'button-timer',
+	templateUrl: './btn-timer-auto-click.component.html',
+	styleUrls: ['./btn-timer-auto-click.component.scss']
 })
 export class BtnTimerAutoClickComponent implements OnInit, OnDestroy {
-  @Input() label = "X";
-  @Input() secondsTimer = 5;
-  // timeLeft: number = 5;
-  progress = 0;
-  timer: any;
-  @Output() timerEnd = new EventEmitter<any>();
+	@Input() label = "X";
+	@Input() secondsTimer = 5;
+	// timeLeft: number = 5;
+	progress = 0;
+	timer: any;
+	@Output() timerEnd = new EventEmitter<any>();
 
-  ngOnInit() {
-    this.startTimer();
-  }
+	ngOnInit() {
+		this.startTimer();
+	}
 
-  click() {
-    this.timerEnd.emit(this.label);
-  }
+	// Called when user manually clicks
+	click() {
+		this.stopTimer();
+		this.timerEnd.emit(this.label);
+	}
 
+	// Helper method to stop the timer
+	private stopTimer() {
+		if (this.timer) {
+			clearInterval(this.timer);
+			this.timer = null;
+		}
+	}
 
-  startTimer() {
-    this.progress = 0;
-    // this.timeLeft = this.secondsTimer; // Reset time
-    const interval = 100; // adjust the interval for smoother progress
-    const totalSeconds = this.secondsTimer;
-    const totalIterations = totalSeconds * 1000 / interval;
-    let iteration = 0;
-    if (this.secondsTimer > 0) {
-      this.timer = setInterval(() => {
-        iteration++;
-        this.progress = (iteration / totalIterations) * 100;
-        // this.timeLeft = totalSeconds - Math.floor(iteration / (1000 / interval));
-        if (iteration >= totalIterations) {
-          clearInterval(this.timer);
-          this.click();
-        }
-      }, interval);
-    }
-  }
-  ngOnDestroy() {
-    this.timer = null;
-  }
+	startTimer() {
+		// Clear any existing timer before starting a new one
+		this.stopTimer();
+
+		this.progress = 0;
+		const interval = 100; // milliseconds
+		const totalSeconds = this.secondsTimer;
+
+		if (totalSeconds > 0) {
+			const totalIterations = (totalSeconds * 1000) / interval;
+			let iteration = 0;
+
+			// Store the timer ID returned by setInterval
+			this.timer = setInterval(() => {
+				iteration++;
+				this.progress = (iteration / totalIterations) * 100;
+
+				if (iteration >= totalIterations) {
+					this.stopTimer();
+					this.timerEnd.emit(this.label);
+				}
+			}, interval);
+		}
+	}
+
+	ngOnDestroy() {
+		this.timer = null;
+	}
 }
